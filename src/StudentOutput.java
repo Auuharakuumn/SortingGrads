b20430423f1d763bb88116c23822b8f7d1bbb39b
Michael Foley <helloworld191@gmail.com>
2015-05-14 09:34:44 -0500

    Outputs basic ordered excel file.

--- src/StudentOutput.java
+++ src/StudentOutput.java
@@ -1,38 +1,25 @@
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class StudentOutput {
 	private String[] orderedStudents;
-	private Workbook workbook;
 
 	public StudentOutput(String[] orderedStudents) {
 		this.orderedStudents = orderedStudents;
 	}
 
-	private int setExcelFile(File file) {
-		Workbook wb;
-
-		try {
-			wb = WorkbookFactory.create(file);
-		} catch (org.apache.poi.openxml4j.exceptions.InvalidFormatException ife) {
-			return 1;
-		} catch (IOException ioe) {
-			return 2;
-		}
-
-		this.workbook = wb;
-
-		return 0;
-	}
-
 	@SuppressWarnings("ResultOfMethodCallIgnored")
-	public void writeExcel() {
+	public void writeExcel() throws FileNotFoundException {
 		File output;
 		String fileName = "output";
 		boolean fileAvailable;
 		int loop = 1;
+		FileOutputStream fos;
 
 		do {
 			fileAvailable = true;
@@ -51,7 +38,9 @@ public class StudentOutput {
 			}
 		} while (!fileAvailable);
 
-		setExcelFile(output);
+		fos = new FileOutputStream(output);
+
+		Workbook workbook = new XSSFWorkbook();
 
 		Sheet sheet = workbook.createSheet();
 
@@ -60,8 +49,14 @@ public class StudentOutput {
 
 			Cell[] c = new Cell[]{r.createCell(0), r.createCell(1)};
 
-			c[0].setCellValue(1);
+			c[0].setCellValue(i);
 			c[1].setCellValue(orderedStudents[i]);
+		}
+
+		try {
+			workbook.write(fos);
+		} catch (IOException ignored) {
+
 		}
 	}
 }
